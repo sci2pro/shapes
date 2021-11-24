f4996bcc9c267dc4b61e6913d06ae9991ea88469
paulkorir <paul@sci2pro.com>
2021-11-24 16:43:09 +0000

    installable

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+# install with 'pip install -e .'
+setup(
+    name='shapes',
+    version='0.1.0',
+    packages=['shapes'],
+    url='https://github.com/sci2pro/shapes',
+    license='Apache-2.0',
+    author='paulkorir',
+    author_email='paul@domain.com',
+    description='Draw shapes in canvas'
+)
--- shapes/tests.py
+++ shapes/tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from shapes import Circle
+from .shapes import Circle
 from math import pi
 
 
