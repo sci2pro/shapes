c27b1f4f2050c696d72b07cb5939ceb9842f060c
paulkorir <paul@sci2pro.com>
2021-11-24 11:26:38 +0000

    circle class defined

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import os
+import sys
+from math import pi
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class Circle:
+    def __init__(self, radius, fill='red', stroke='black'):
+        self._radius = radius # private/protected
+        self._fill = fill
+        self._stroke = stroke
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    @property
+    def radius(self): # public access for _radius; read-only
+        return self._radius
+
+    def calculate_area(self):
+        """Calculates the area"""
+        return pi * self._radius ** 2
+
+    @property
+    def area(self):
+        return self.calculate_area()
+
+    def __len__(self):
+        return int(2 * pi * self._radius)
+
+    def __call__(self):
+        return "I am a circle!"
+
+
+def main():
+    circle = Circle(5.0, fill='orange', stroke='red')
+    print(f"area = {circle.calculate_area()}")
+    print(f"area = {circle.area}")
+    print(f"circumference is {len(circle)}")
+    print(circle())
+    return os.EX_OK
+
+
+if __name__ == "__main__":
+    sys.exit(main())
